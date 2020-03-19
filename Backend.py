7da0f5dc5427123969acd7bf1d8480c511473009
Kshitij Sangar <kshitijsangar@gmail.com>
2020-03-19 14:40:50 +0530

    Very First Commit

---
+++ Backend.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+"""
+Variables for File: fmain, (Contains the file with all Acesis)
+                    f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12 (Contains monthly data)
+"""
+
+fmain = pd.ExcelFile("Excel_Files/" + fmain)
--- README.md
+++
@@ -1,2 +0,0 @@
-# GST_miniproject
-This is miniproject in which the program complies the data from 12 different files and adds particular columns in a single file.
