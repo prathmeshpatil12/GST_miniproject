5191aa54816fe42f47eddce92ad1955064277366
Kshitij Sangar <kshitijsangar@gmail.com>
2020-03-19 17:33:53 +0530

    :=Seconf Commit

--- Backend.py
+++ Backend.py
@@ -1,8 +1,26 @@
 import pandas as pd
+import xlsxwriter
 
 """
 Variables for File: fmain, (Contains the file with all Acesis)
                     f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12 (Contains monthly data)
 """
 
-fmain = pd.ExcelFile("Excel_Files/" + fmain)
+def Backend_Cal(lst):
+    fmain = pd.ExcelFile("Excel_Files/" + str(lst[0]))
+    for s in fmain.sheet_names:
+        df_main = fmain.parse(s)
+        fmain_workbook = xlsxwriter.Workbook("Excel_Files/" + str(lst[0]))
+        fmain_Wsheet = fmain_workbook.add_worksheet()
+        for file in lst[1:]:
+            f = pd.ExcelFile("Excel_Files/" + str(file))
+            writer = pd.ExcelWriter("Excel_Files/" + str(lst[0]))
+            for sheet in f.sheet_names:
+                df = f.parse(sheet)
+                print(len(df.columns) )
+                print(len(df_main.columns))
+                fmain_Wsheet.write(0, len(df.columns)+1 ,'hello')
+
+
+l = ['Full_demo.xlsx', 'April_demo.xlsx']
+Backend_Cal(l)
---
+++ Frontend.py
@@ -0,0 +1,5 @@
+from . import Backend
+
+l = ['Full_demo.xlsx', 'April_demo.xlsx']
+
+Backend.Backend_Cal(l)
