ec8f13a531dd66de56cb3a0761d752d820309c12
Kshitij Sangar <kshitijsangar@gmail.com>
2020-03-20 15:12:56 +0530

    Third Commit

--- Backend.py
+++ Backend.py
@@ -8,19 +8,53 @@ Variables for File: fmain, (Contains the file with all Acesis)
 
 def Backend_Cal(lst):
     fmain = pd.ExcelFile("Excel_Files/" + str(lst[0]))
-    for s in fmain.sheet_names:
+    for s in fmain.sheet_names:   # Traversing the sheets of the main file
         df_main = fmain.parse(s)
-        fmain_workbook = xlsxwriter.Workbook("Excel_Files/" + str(lst[0]))
-        fmain_Wsheet = fmain_workbook.add_worksheet()
-        for file in lst[1:]:
+        for file in lst[1:]:  # Traversing all the files except the main file.
             f = pd.ExcelFile("Excel_Files/" + str(file))
-            writer = pd.ExcelWriter("Excel_Files/" + str(lst[0]))
+
+            #writer = pd.ExcelWriter("Excel_Files/" + "demo.xlsx")  # str(lst[0]
             for sheet in f.sheet_names:
                 df = f.parse(sheet)
-                print(len(df.columns) )
-                print(len(df_main.columns))
-                fmain_Wsheet.write(0, len(df.columns)+1 ,'hello')
 
 
+
+                #--------------------- YOUR CODE HERE ---------------------#
+
+
+
+
+
+
+                """
+                    for i in range(len(df):
+                        df['CSGT'][i] --> will print the value in that particular row
+                
+                """
+
+
+
+
+
+
+                # -------------------------- XXX --------------------------#
+
+
+
+
+                df_main.insert(len(df_main.columns), "CGST", df_main.shape[0] * [0])
+                df_main.insert(len(df_main.columns), "SGST/UGST", df_main.shape[0] * [0])
+                df_main.insert(len(df_main.columns), "IGST", df_main.shape[0] * [0])
+                df_main.insert(len(df_main.columns), "CESS", df_main.shape[0] * [0])
+
+
+                #df['CGST'] = df.shape[0]*[0]
+
+                print(df_main)
+
+        # SAVING THE FINAL FILE
+        df_main.to_excel("Excel_Files/demo.xlsx", 'sheet1')
+
+# Temperory function call with demo excel files.
 l = ['Full_demo.xlsx', 'April_demo.xlsx']
 Backend_Cal(l)
